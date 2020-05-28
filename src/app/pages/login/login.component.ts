6934262f0387bfbac67fb440c345d704f1a38da4
Gache <erickfrancodelgado@hotmail.com>
2020-05-28 16:02:38 +0200

    validation du login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { UtilisateurModel } from '../../models/utilisateur.model';
+import { NgForm } from '@angular/forms';
 
-@Component({
+
+@Component( {
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
-})
+} )
 export class LoginComponent implements OnInit {
 
+  utilisateur: UtilisateurModel = new UtilisateurModel();
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit () {
+  }
+
+  UtilisateurLogin ( form: NgForm ) {
+    if ( form.invalid ) {
+      return;
+    }
+
+    console.log( this.utilisateur );
+    console.log( form );
+
+
   }
 
 }
